58f27176828f15e44933eb34f6b888efe7f37153
andtho89 <gunanto.simamora@gmail.com>
2022-08-25 11:16:43 +0700

    version 5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 setup(
     name='pc_monitoring',
-    version='0.0.4',
+    version='0.0.5',
     description='Monitoring computer realtime with socket',
     long_description=long_description,
     long_description_content_type='text/markdown',
